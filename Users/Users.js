8c9eb5a5070b3b563db4fa341aa62f20ab04b8f9
Antonio <khaingjar@gmail.com>
2022-03-30 22:06:13 +0630

    make it better

--- Users/Users.js
+++ Users/Users.js
@@ -18,8 +18,7 @@ const removeUser = (id) =>{
     const index = users.findIndex(user => user.id === id);
     if(index !== -1){
         return users.splice(index,1)[0];
-      
-       
+ 
     }
 }
 
