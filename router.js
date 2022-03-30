b6e05b25e05cac7d303af6d5cd09a8182b9dc37b
Antonio <khaingjar@gmail.com>
2022-03-30 15:18:56 +0630

    second commit

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express =require('express');
 const router = express.Router();
 
 router.get('/',(req,res) =>{
-    res.send('Home Page');
+    res.send('Server running ...');
 })
 
 module.exports = router;
