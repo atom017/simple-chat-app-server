3726cb9371c447e24ec04b8090a969195e0380c4
Antonio <khaingjar@gmail.com>
2022-03-30 15:56:06 +0630

    make it better

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server,{
     cors: {
-        origin: "http://localhost:3000",
+        origin: ["http://localhost:3000","https://nf-simple-chat-app.netlify.app/"],
        
       }
 });
